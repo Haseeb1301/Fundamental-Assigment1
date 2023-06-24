29d793773a6df504b83d4ef977885b783e2e7d27
Haseeb1301 <136188993+Haseeb1301@users.noreply.github.com>
2023-06-24 22:46:59 +0500

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,96 @@
+# # Answer 1
+# a = [1, 2, 4, -1, 6, 0, 5]
+# def sumofarray(a):
+#     sum=0
+#     for i in range (len(a)):
+#         sum += a[i]
+#     return sum
+# print ("Answer 1 =",  sumofarray(a) )
+
+###################################################
+ 
+#  # Answer 2
+# def rev_str(text):
+#     result = ""
+#     for i in text:
+#         result = i + result
+#     return result
+# print (("Answer 2 ="), rev_str("Hello, World!"))
+
+####################################################
+
+# # Answer 3
+# list = [1, 2, 3, 4, 5]
+# print("Original list :", list)
+# reverse_list = []
+# for value in list:
+#     reverse_list = [value] + reverse_list
+# print (("Answer 3 ="), ("List after reverse : ", reverse_list))
+
+#####################################################
+
+# # Answer 4
+# str1 = "racecar"
+# str2 = "lahore"
+# w = ""
+# for i in str2:
+#     w = i + w
+
+# if (str2 == w):
+#     print("Yes")
+# else:
+#     print("No")
+
+##################################################
+
+# # Answer 5
+# input5 = [8, 3, 10, 5, 2]
+
+# def largest(input5, n):
+#     max = input5[0]
+#     for i in range(1, n):
+#         if input5[i] > max:
+#             max = input5[i]
+#     return max
+# n = len(input5)
+# output = largest(input5, n)
+# print(input5)
+# print("Answer 5 : Largest array is :", output)
+
+####################################################
+
+# # Answer 6
+# list = [1, 2, 3, 2, 4, 3, 5]
+# print ("The original list is :" + str(list))
+
+# new_list = []
+# for i in list:
+#     if i not in new_list:
+#         new_list.append(i)
+# print ("Answer 6 : The update list is :" +str(new_list)) 
+
+####################################################
+
+## Answer 7
+# num = 29
+# num = int(input("Enter number :"))
+# show = False
+
+# if num == 1:
+#     print (num, "is not a prime number")
+# elif num > 1:
+#     for i in range(2, num):
+#         if (num % i) == 0:
+#             show = True
+#             break
+
+#     if show:
+#         print (num, "is not a prime number, Answer 7")
+#     else:
+#         print (num, "is a prime number, Answer 7")
+
+#########################################################
+
+# # Answer 8
+# print("I can't understand Question # 8")
+
